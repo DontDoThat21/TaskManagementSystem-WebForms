b8966e7d685995f100d6388e089ac0ef7bef5b45
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-18 13:40:35 -0400

    Added Entity Framework reference. Added SQL Server Entity Framework reference. Added ASP.NET Identity reference. Added IdentityModels (for ASP.NET Identity) class and created baseline ApplicationUser.cs user model. Added standard DTO/DAO Models folder, with initial "Task" model. Added Account, Tasks, Dashboard folders.

---
+++ TaskManagementSystem-WebForms/IdentityModels/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace TaskManagementSystem_WebForms.IdentityModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
